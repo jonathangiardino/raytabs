6e8ece7e685c04b49dec9400edf3976d741bb057
Jonathan Giardino <j.giardino@me.com>
2023-10-15 15:47:12 +0200

    Create admin tag

---
+++ src/components/AdminTag.tsx
@@ -0,0 +1,7 @@
+export default function AdminTag() {
+  return (
+    <span className="ml-auto bg-[var(--admin-label-bg)] text-xs text-[var(--admin-label-text)] px-2 py-1 rounded-[2px]">
+      Admin
+    </span>
+  );
+} 
