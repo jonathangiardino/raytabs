89a95517a7e72ddd5f905e7536e2b698233a6129
Jonathan Giardino <j.giardino@me.com>
2023-10-15 21:31:19 +0200

    Handle admin button visibility on small screens

--- src/components/AdminButton.tsx
+++ src/components/AdminButton.tsx
@@ -20,7 +20,7 @@ export default function AdminButton({
       <button
         tabIndex={-1}
         onClick={handleCheckboxChange}
-        className="hidden group-focus-visible:hidden md:group-hover:flex items-center md:group-hover:gap-1 ml-auto bg-[var(--admin-set-btn)] md:hover:opacity-80 text-[11px] px-2 py-1 rounded-[2px] animate-fade-in"
+        className="flex gap-1 md:hidden group-focus-visible:hidden md:group-hover:flex items-center ml-auto bg-[var(--admin-set-btn)] md:hover:opacity-80 text-[11px] px-2 py-1 rounded-[2px] animate-fade-in"
       >
         Set admin <PlusIcon height={13} width={13} />
       </button>
