2c7ed466d6f44d9252bd0341378cc47bc0262a03
Jonathan Giardino <j.giardino@me.com>
2023-10-15 15:46:23 +0200

    Add keyframe animations and global variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,21 @@ const config: Config = {
       fontSize: {
         sm: ["13px", "16px"],
       },
+      animation: {
+        "slide-right": "slideRightAndBack 0.3s ease-in-out",
+        "fade-in": "fadeIn 0.3s ease-in-out",
+      },
+      keyframes: {
+        slideRightAndBack: {
+          "0%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(50px)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
