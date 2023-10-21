36fe1a063d0a0f0d42e0690be9d249d0e01c6484
Jonathan Giardino <j.giardino@me.com>
2023-10-21 21:16:08 +0200

    Remove old components

--- src/components/UserListLegend.tsx
+++
@@ -1,7 +0,0 @@
-export default function UserListLegend({ label }: { label: string }) {
-  return (
-    <legend className="px-2 pb-1 text-xs font-medium text-[var(--foreground-alt)]">
-      {label}
-    </legend>
-  );
-}
